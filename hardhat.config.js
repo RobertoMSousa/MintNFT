19b4a4ee70cb4c5bfad3d94556695c7d2a15a766
Roberto Sousa <beto.sousa22@gmail.com>
2024-02-04 23:26:01 +0000

    add new networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,26 @@
-require("dotenv").config();
+require('dotenv').config();
+
 require("@nomicfoundation/hardhat-toolbox");
+
+const { API_SEPOLIA_URL, PRIVATE_KEY, API_GOERLI_URL, API_MAINNET_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.19",
+  defaultNetwork: "sepolia",
   networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_SEPOLIA_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
     goerli: {
-      url: process.env.GOERLI_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: API_GOERLI_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: API_MAINNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
-  }
+  },
 };
